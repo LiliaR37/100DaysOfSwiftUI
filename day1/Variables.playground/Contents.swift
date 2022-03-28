b3ae0393a0299b3c9d38f5eb253750e0e18d08d6
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-03-28 14:41:57 -0300

    Day 1

---
+++ day1/Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
