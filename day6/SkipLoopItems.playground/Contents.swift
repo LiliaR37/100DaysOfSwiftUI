2e8130a4e3df3fc837a5594400c2f8b7688cd67e
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-04-02 10:01:03 -0300

    Day 6 & checkpoint 3 done

---
+++ day6/SkipLoopItems.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+/*
+ So, use continue when you want to skip the rest of the current loop iteration, and use break when you want to skip all remaining loop iterations
+ */
