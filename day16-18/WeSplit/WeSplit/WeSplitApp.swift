61379eea186f844f8972fc85a3c6b36da88ded92
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-04-11 11:42:45 -0300

    Project 1

---
+++ day16-18/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by LILIA MARIANGEL on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
