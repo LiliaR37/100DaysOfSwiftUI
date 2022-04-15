75d2bdc94f71be8631317479dfda57e3640e1946
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-04-15 08:50:19 -0300

    Project 2

---
+++ day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by LILIA MARIANGEL on 04/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.green
+    
+            Text("Hello, world!")
+                .padding()
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day20/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by LILIA MARIANGEL on 04/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
