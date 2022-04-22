cfedae80ef00d9978b94c5d593359a16b1f3323a
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-04-22 14:57:32 -0300

    Project 4

---
+++ day26-28/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by LILIA MARIANGEL on 04/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
