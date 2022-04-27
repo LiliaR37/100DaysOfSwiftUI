f90b9e575e0a08c8289b58f2e9787642b2205017
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-04-27 13:03:38 -0300

    Project 6

---
+++ day32-34/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by LILIA MARIANGEL on 04/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ day32-34/Animations/Animations/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by LILIA MARIANGEL on 04/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+@State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            animationAmount += 1
+            
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
