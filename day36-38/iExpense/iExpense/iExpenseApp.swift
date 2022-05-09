1ae0f72a82d20522e684542d89c349bf27f37035
LiliaR37 <lalocadeloslentes14@gmail.com>
2022-05-09 11:16:09 -0300

    Project 7

---
+++ day36-38/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by LILIA MARIANGEL on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
